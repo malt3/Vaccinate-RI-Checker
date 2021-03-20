960caab6e9be19eed89c1d5cf7ac6282681875e1
Malte Poll <malte.poll@web.de>
2021-03-20 21:37:41 +0100

    Implemented regular checking and notifications via pushover

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+import time
+from copy import deepcopy
 from functools import reduce
 
 import requests
@@ -5,6 +8,9 @@ from bs4 import BeautifulSoup
 
 SCHEME = 'https://'
 HOSTNAME = 'www.vaccinateri.org'
+INTERVAL = 60
+PUSHOVER_USER_KEY = ''
+PUSHOVER_API_KEY = ''
 
 
 class SearchResultItem:
@@ -85,70 +91,132 @@ class ClinicWithFreeTimeslots(SearchResultItem):
         return ClinicWithFreeTimeslots(name=search_result_item.name, address=search_result_item.address, vaccinations_offered=search_result_item.vaccinations_offered, age_groups_served=search_result_item.age_groups_served, services_offered=search_result_item.services_offered, additional_information=search_result_item.additional_information, clinic_hours=search_result_item.clinic_hours, appointments_available=search_result_item.appointments_available, special_instructions=search_result_item.special_instructions, clinic_id=search_result_item.clinic_id)
 
 
-def clinic_search(location='', search_radius='All', venue_search_name_or_venue_name_i_cont='', clinic_date_eq_year='',
-                  clinic_date_eq_month='', clinic_data_eq_day='', vaccinations_name_i_cont='', commit='Search'):
-    payload = {
-        'location': location,
-        'search_radius': search_radius,
-        'q[venue_search_name_or_venue_name_i_cont]': venue_search_name_or_venue_name_i_cont,
-        'clinic_date_eq[year]': clinic_date_eq_year,
-        'clinic_date_eq[month]': clinic_date_eq_month,
-        'clinic_date_eq[day]': clinic_data_eq_day,
-        'q[vaccinations_name_i_cont]': vaccinations_name_i_cont,
-        'commit': commit
-    }
-    r = requests.get(f'{SCHEME}{HOSTNAME}/clinic/search', params=payload)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    search_results = soup.find('div', {'class': 'main-container'}).findChild('div', {
-        'class': ['mt-24', 'border-t', 'border-gray-200']}).findChildren('div', {
-        'class': ['md:flex', 'justify-between', '-mx-2', 'md:mx-0', 'px-2', 'md:px-4', 'pt-4', 'pb-4', 'border-b',
-                  'border-gray-200']})
-    search_result_items = []
-    for item in search_results:
-        search_result_items.append(SearchResultItem.fromHTML(item))
-    return search_result_items
+class DifferentialVaccinationAppointmentChecker:
+    def __init__(self):
+        self.clinic_id_map = {}
 
+    @staticmethod
+    def client_registration(clinic_id):
+        payload = {
+            'clinic_id': clinic_id
+        }
+        timeslots = []
+        r = requests.get(f'{SCHEME}{HOSTNAME}/client/registration', params=payload, allow_redirects=False)
+        if r.status_code == 302 and r.headers[
+            'location'] == 'https://www.vaccinateri.org/errors?message=Clinic+does+not+have+any+appointment+slots+available.':
+            print(f'clinic_id {clinic_id} has no appointments available')
+            return []
+        elif r.status_code != 200:
+            print(f'Client registration for clinic_id {clinic_id} returned unexpected status code: {r.status_code}')
+            return []
+        else:
+            soup = BeautifulSoup(r.text, 'html.parser')
+            appointments_table = soup.find(id='appointments-section').div.table
+            appointment_trs = appointments_table.tbody.findAll('tr', recursive=False)
+            for appointment_tr in appointment_trs:
+                timeslot = Timeslot.fromHTML(appointment_tr)
+                if timeslot.available:
+                    timeslots.append(timeslot)
+        return timeslots
 
-def client_registration(clinic_id):
-    payload = {
-        'clinic_id': clinic_id
-    }
-    timeslots = []
-    r = requests.get(f'{SCHEME}{HOSTNAME}/client/registration', params=payload, allow_redirects=False)
-    if r.status_code == 302 and r.headers['location'] == 'https://www.vaccinateri.org/errors?message=Clinic+does+not+have+any+appointment+slots+available.':
-        print(f'clinic_id {clinic_id} has no appointments available')
-        return []
-    elif r.status_code != 200:
-        print(f'Client registration for clinic_id {clinic_id} returned unexpected status code: {r.status_code}')
-        return []
-    else:
+    @staticmethod
+    def clinic_search(location='', search_radius='All', venue_search_name_or_venue_name_i_cont='',
+                      clinic_date_eq_year='',
+                      clinic_date_eq_month='', clinic_data_eq_day='', vaccinations_name_i_cont='', commit='Search'):
+        payload = {
+            'location': location,
+            'search_radius': search_radius,
+            'q[venue_search_name_or_venue_name_i_cont]': venue_search_name_or_venue_name_i_cont,
+            'clinic_date_eq[year]': clinic_date_eq_year,
+            'clinic_date_eq[month]': clinic_date_eq_month,
+            'clinic_date_eq[day]': clinic_data_eq_day,
+            'q[vaccinations_name_i_cont]': vaccinations_name_i_cont,
+            'commit': commit
+        }
+        r = requests.get(f'{SCHEME}{HOSTNAME}/clinic/search', params=payload)
         soup = BeautifulSoup(r.text, 'html.parser')
-        appointments_table = soup.find(id='appointments-section').div.table
-        appointment_trs = appointments_table.tbody.findAll('tr', recursive=False)
-        for appointment_tr in appointment_trs:
-            timeslot = Timeslot.fromHTML(appointment_tr)
-            if timeslot.available:
-                timeslots.append(timeslot)
-    return timeslots
+        search_results = soup.find('div', {'class': 'main-container'}).findChild('div', {
+            'class': ['mt-24', 'border-t', 'border-gray-200']}).findChildren('div', {
+            'class': ['md:flex', 'justify-between', '-mx-2', 'md:mx-0', 'px-2', 'md:px-4', 'pt-4', 'pb-4', 'border-b',
+                      'border-gray-200']})
+        search_result_items = []
+        for item in search_results:
+            search_result_items.append(SearchResultItem.fromHTML(item))
+        return search_result_items
 
+    def update(self, callback):
+        search_result_items = DifferentialVaccinationAppointmentChecker.clinic_search()
+        clinics_with_appointments_available_according_to_search = [item for item in search_result_items if
+                                                                   item.appointments_available > 0]
+        total_appointments_available_according_to_search = reduce(lambda x, y: x + y.appointments_available,
+                                                                  clinics_with_appointments_available_according_to_search,
+                                                                  0)
+        clinics_with_free_timeslots = []
+        if total_appointments_available_according_to_search > 0:
+            print(f'Search says there are a total of {total_appointments_available_according_to_search} appointments available from {len(clinics_with_appointments_available_according_to_search)} different clinics')
+        for clinic_with_appointments_available in clinics_with_appointments_available_according_to_search:
+            timeslots = DifferentialVaccinationAppointmentChecker.client_registration(clinic_with_appointments_available.clinic_id)
+            if len(timeslots) > 0:
+                clinic_with_free_timeslots = ClinicWithFreeTimeslots.fromSearchResultItem(
+                    clinic_with_appointments_available)
+                clinic_with_free_timeslots.timeslots.extend(timeslots)
+                clinics_with_free_timeslots.append(clinic_with_free_timeslots)
+
+        old_clinic_id_map = deepcopy(self.clinic_id_map)
+        self.clinic_id_map = {}
+        for clinic_with_free_timeslots in clinics_with_free_timeslots:
+            self.clinic_id_map[clinic_with_free_timeslots.clinic_id] = clinic_with_free_timeslots
+            print(
+                f'Clinic {clinic_with_free_timeslots.name} id: {clinic_with_free_timeslots.clinic_id} offers the following free timeslots at {SCHEME}{HOSTNAME}/client/registration?clinic_id={clinic_with_free_timeslots.clinic_id}:')
+            for timeslot in clinic_with_free_timeslots.timeslots:
+                print(f'    {timeslot.timestr}')
+
+        for clinic_id in self.clinic_id_map.keys():
+            new_timeslots = []
+            if clinic_id in old_clinic_id_map:
+                for timeslot in self.clinic_id_map[clinic_id].timeslots:
+                    is_new = True
+                    for timeslot_old in old_clinic_id_map[clinic_id].timeslots:
+                        if timeslot.unixtime == timeslot_old.unixtime:
+                            is_new = False
+                            break
+                    if is_new:
+                        new_timeslots.append(timeslot)
+            else:
+                new_timeslots.extend(self.clinic_id_map[clinic_id].timeslots)
+
+            if len(new_timeslots) > 0:
+                callback(self.clinic_id_map[clinic_id], new_timeslots)
+
+
+def printCallback(clinic, timeslots):
+    print(f'{len(timeslots)} new timeslots for {clinic.name} found: {SCHEME}{HOSTNAME}/client/registration?clinic_id={clinic.clinic_id}')
+
+def pushoverCallback(clinic, timeslots):
+    try:
+        r = requests.post("https://api.pushover.net/1/messages.json", data={
+            "token": PUSHOVER_API_KEY,
+            "user": PUSHOVER_USER_KEY,
+            "message": f'{len(timeslots)} new timeslots for {clinic.name} found: {SCHEME}{HOSTNAME}/client/registration?clinic_id={clinic.clinic_id}',
+            "url": f'{SCHEME}{HOSTNAME}/client/registration?clinic_id={clinic.clinic_id}',
+        })
+    except:
+        pass
 
 if __name__ == '__main__':
-    search_result_items = clinic_search()
-    clinics_with_appointments_available_according_to_search = [item for item in search_result_items if item.appointments_available > 0]
-    total_appointments_available_according_to_search = reduce(lambda x, y: x + y.appointments_available,
-                                                              clinics_with_appointments_available_according_to_search, 0)
-    clinics_with_free_timeslots = []
-    print(
-        f'Search says there are a total of {total_appointments_available_according_to_search} appointments available from {len(clinics_with_appointments_available_according_to_search)} different clinics')
-    for clinic_with_appointments_available in clinics_with_appointments_available_according_to_search:
-        timeslots = client_registration(clinic_with_appointments_available.clinic_id)
-        if len(timeslots) > 0:
-            clinic_with_free_timeslots = ClinicWithFreeTimeslots.fromSearchResultItem(clinic_with_appointments_available)
-            clinic_with_free_timeslots.timeslots.extend(timeslots)
-            clinics_with_free_timeslots.append(clinic_with_free_timeslots)
-
-    for clinic_with_free_timeslots in clinics_with_free_timeslots:
-        print(f'Clinic {clinic_with_free_timeslots.name} id: {clinic_with_free_timeslots.clinic_id} offers the following free timeslots at {SCHEME}{HOSTNAME}/client/registration?clinic_id={clinic_with_free_timeslots.clinic_id}:')
-        for timeslot in clinic_with_free_timeslots.timeslots:
-            print(f'    {timeslot.timestr}')
+    if len(sys.argv) >= 3:
+        PUSHOVER_USER_KEY = sys.argv[1]
+        PUSHOVER_API_KEY = sys.argv[2]
+        callback = pushoverCallback
+    else:
+        print("Please supply pushover user and api keys as arguments to this script in order to allow pushover to notify you")
+        print(f'Usage: {sys.argv[0]} PUSHOVER_USER_KEY PUSHOVER_API_KEY')
+        callback = printCallback
+    checker = DifferentialVaccinationAppointmentChecker()
+    while True:
+        try:
+            checker.update(callback)
+        except Exception as e:
+            print(f'Update failed: {e}')
+        time.sleep(INTERVAL)
 
